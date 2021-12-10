0d55123d5778cd338b841d21124f68d96e9fcca1
huyanh <phanhuyanh251@gmail.com>
2021-12-10 11:46:36 +0700

    [config] webpack

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      chunks: ['main']
     }),
     new VueLoaderPlugin()
   ],
